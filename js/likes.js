1c43a24fda35a0d4a5f995b6aa50250c4d603a95
Greg <gregthue@gmail.com>
2021-10-14 11:04:18 +0200

    fonction like fonctionnelle pour l’incrémentation

--- js/likes.js
+++ js/likes.js
@@ -1,37 +1,26 @@
-
 function updateLike(isLiked) {
-    const nbLikes = document.querySelectorAll(".numberOfLikes");
-    const hearts = document.querySelectorAll(".fa-heart");
-  
-    for (let i = 0; i < nbLikes.length; i++) {
-        let mediaNbLike = nbLikes[i].textContent ;
-        
-        console.log(mediaNbLike++)
+  const nbLikes = document.querySelectorAll(".numberOfLikes");
+  const hearts = document.querySelectorAll(".fa-heart");
 
-    }
-  
-    for (let i = 0; i < hearts.length; i++) {
-        isLiked = hearts[i].getAttribute("aria-label");
-        console.log(isLiked)
-    }
-    
-    hearts.forEach(heart => {
-      heart.addEventListener('click', () => {
+  for (let i = 0; i < hearts.length; i++) {
+    isLiked = hearts[i].getAttribute("aria-label");
+  }
+
+  hearts.forEach((heart) => {
+    heart.addEventListener("click", () => {
+      for (let i = 0; i < nbLikes.length; i++) {
+        let mediaNbLike = nbLikes[i].textContent;
         if (isLiked == "unliked") {
-     
-            //heart[i].setAttribute("aria-label","liked")/* Il faut récuperer la valeur du span like puis 
-            //selon le label augmenter ou baisser le nb et également penser au nb de likes total */
-  
-          console.log("liker")
+          mediaNbLike++;
+          nbLikes[i].innerHTML = mediaNbLike;
+          heart.setAttribute("aria-label", "liked");
         }
         if (isLiked == "liked") {
-           
-            //heart[i].setAttribute("aria-label","unliked")
+          mediaNbLike--;
+          nbLikes[i].innerHTML = mediaNbLike;
+          heart.setAttribute("aria-label", "unliked");
         }
-        console.log("click")
-      });
-      
+      }
     });
-    
-  }
-  
+  });
+}
